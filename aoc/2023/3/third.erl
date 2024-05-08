307037ad88db11405cadbe990980003ab05db489
anatoly@kapkanov.com <anatoly@kapkanov.com>
2024-05-08 17:59:58 +0000

    resolve merge hell
    FossilOrigin-Name: e630fe336a1dd3bd854b1db36cfc2650094f38ebecc95f8335e5e6cd26079b31
    
    FossilOrigin-Name: cff90ac8cf3bfbc6308e5edcf85012ea1260a4d215c77a4d3b8c73c2ac5572b0

--- aoc/2023/3/third.erl
+++ aoc/2023/3/third.erl
@@ -35,21 +35,6 @@ getcol([Head | Rest], Column) ->
 getcell(List, Row, Column) ->
   getcol(getrow(List, Row), Column).
 
-setcell(List, Row, Column, Value) ->
-
-setcell(Res)
-
-% walk(List) ->
-%   R = 0,
-%   C = 0,
-% walk(R, C, Rows, Columns) ->
-%   if
-%     R < 0 orelse C < 0 orelse R >= rows orelse C >= Columns ->
-%       false;
-%     true ->
-%       true
-%   end.
-
 genpath(Rows, Columns) ->
   genpath(Rows, Columns, []).
 genpath(0, Columns, Res) ->
@@ -71,9 +56,41 @@ listlen([], Res) ->
 listlen([_ | Rest], Res) ->
   listlen(Rest, Res + 1).
 
+until([], _) ->
+  [];
+until(Head, 0) ->
+  [];
+until([Head | Rest], Count) ->
+  [Head | until(Rest, Count - 1)].
+
+% Starting from 0, index Count is included
+rest(List, 0) ->
+  List;
+rest([], _) ->
+  [];
+rest([Head | Rest], Count) ->
+  rest(Rest, Count - 1).
+
+% Starting from 0
+setcol([], _, Value) ->
+  [];
+setcol(Str, 0, Value) ->
+  Str;
+setcol(Str, Column, Value) ->
+  Before     = until(Str, Column),
+  [_ | Rest] = rest(Str, Column),
+  After      = [Value | Rest],
+  lists:merge(Before, After).
+
+setcell(List, Row, Column, Value) ->
+  Before        = until(List, Row),
+  [Head | Rest] = rest(List, Row),
+  Changed       = [setcol(Head, Column, Value)],
+  lists:merge(lists:merge(Before, Changed), Rest).
+
 start() ->
   {ok, Bin} = file:read_file("test.txt"),
-  Str = case unicode:characters_to_list(Bin) of
+  Str       = case unicode:characters_to_list(Bin) of
     L when is_list(L) ->
       strmod:crlf2lf(L);
     _ ->
@@ -86,15 +103,25 @@ start() ->
   io:format("Columns = ~p~n", [Columns]),
   io:format("Rows = ~p~n", [Rows]),
 
-  Path = genpath(Rows, Columns),
+  Path       = genpath(Rows, Columns),
   io:format("Path = ~p~n~n", [Path]),
 
-  Row  = getrow(List, 3),
-  Col  = getcol(Row, 6),
+  Row        = getrow(List, 3),
+  Col        = getcol(Row, 6),
   io:format("List = ~p~n", [List]),
   io:format("Row  = ~p~n", [Row]),
   io:format("Col  = ~c~n", [Col]),
   io:format("Get  = ~c~n", [getcell(List, 3, 6)]),
   io:format("genrow(10)  = ~p~n", [genrow(10)]),
   io:format("genpath(3,4)  = ~p~n", [genpath(3, 4)]),
-  io:format("listlen(List)  = ~p~n", [listlen(List)]).
+  io:format("listlen(List)  = ~p~n", [listlen(List)]),
+
+  io:format("setcol(\"123456\", 3, $9) = ~p~n", [setcol("123456", 3, $9)]),
+
+  io:format("until(\"123456\", 3)  = ~p~n", [until("12345678", 3)]),
+  io:format("until([\"123\",\"456\",\"hola\"], 2)  = ~p~n", [until(["123","456","hola"], 2)]),
+  io:format("getrow([\"123\",\"456\",\"hola\"], 2)  = ~p~n", [getrow(["123","456","hola"], 2)]),
+
+  io:format("setcell([[1,2,3],[4,5,6],[7,8,9]], 1, 1, 11) = ~p~n", [setcell([[1,2,3],[4,5,6],[7,8,9]], 1, 1, 11)]),
+
+  io:format("rest([1,2,3,4,5,6], 4) = ~p~n", [rest([1,2,3,4,5,6], 4)]).
