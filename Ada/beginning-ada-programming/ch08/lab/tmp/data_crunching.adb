c7edb8b1ce84b647545c47c9da9f53363cd62b7f
anatoly <anatoly@kapkanov.com>
2025-12-15 08:14:08 +0000

    ada(ch08): complete 3/3 lab tasks
    FossilOrigin-Name: aea89658264aa24e3dcea294e121c9e2afceda36bae832c0ef43b63b3fa40601

--- Ada/beginning-ada-programming/ch08/lab/tmp/data_crunching.adb
+++ Ada/beginning-ada-programming/ch08/lab/tmp/data_crunching.adb
@@ -4,8 +4,6 @@ with Ada.Directories;
 use  type Ada.Directories.File_Kind;
 
 procedure data_crunching is
-  File_Name     : String(1 .. 64);
-  File_Name_Len : Natural := 0;
   File_Handle   : Ada.Text_IO.File_Type;
 
 begin
@@ -14,43 +12,63 @@ begin
     return;
   end if;
 
-  File_Name_Len                 := Ada.Command_Line.Argument(1)'Length;
-  File_Name(1 .. File_Name_Len) := Ada.Command_Line.Argument(1);
+  declare
+    File_Name : String := Ada.Command_Line.Argument(1);
+  begin
 
-  if 
-    Ada.Directories.Exists(File_Name(1 .. File_Name_Len))
-    and then Ada.Directories.Kind(File_Name(1 .. File_Name_Len)) = Ada.Directories.Ordinary_File
-  then
-    Ada.Text_IO.Open(
-      File => File_Handle,
-      Mode => Ada.Text_IO.In_File,
-      Name => File_Name(1 .. File_Name_Len)
-    );
-  else
-    Ada.Text_IO.Put_Line("File " & File_Name(1 .. File_Name_Len) & " not found");
-    return;
-  end if;
+    if 
+      Ada.Directories.Exists(File_Name)
+      and then Ada.Directories.Kind(File_Name) = Ada.Directories.Ordinary_File
+    then
+      Ada.Text_IO.Open(
+        File => File_Handle,
+        Mode => Ada.Text_IO.In_File,
+        Name => File_Name
+      );
+    else
+      Ada.Text_IO.Put_Line("File " & File_Name & " not found");
+      return;
+    end if;
 
-  while not Ada.Text_IO.End_Of_File(File_Handle) loop
     declare
-      Line : String := Ada.Text_IO.Get_Line(File_Handle);
+      File_Name_Out   : String := Ada.Text_IO.Get_Line;
+      File_Handle_Out : Ada.Text_IO.File_Type;
     begin
-      for j in Line'Range loop
-        if 
-          Line(j) = 'A'
-          and then j + 2 <= Line'Length
-        then
-          if Line(j .. j + 2) = "Ada" then
-            Line(j .. j + 2) := "ADA";
-          end if;
-        end if;
-        Ada.Text_IO.Put(Line(j));
-      end loop;
-      Ada.Text_IO.New_Line;
-    end;
-  end loop;
+      Ada.Text_IO.Create(
+        File => File_Handle_Out,
+        Mode => Ada.Text_IO.Out_File,
+        Name => File_Name_Out
+      );
 
-  Ada.Text_IO.Close(File => File_Handle);
+      while not Ada.Text_IO.End_Of_File(File_Handle) loop
+        declare
+          Line : String := Ada.Text_IO.Get_Line(File_Handle);
+        begin
+          for j in Line'Range loop
+            if 
+              Line(j) = 'A'
+              and then j + 2 <= Line'Length
+            then
+              if Line(j .. j + 2) = "Ada" then
+                Line(j .. j + 2) := "ADA";
+              end if;
+            end if;
+            -- Ada.Text_IO.Put(Line(j));
+            Ada.Text_IO.Put(
+              File => File_Handle_Out,
+              Item => Line(j)
+            );
+          end loop;
+          -- Ada.Text_IO.New_Line;
+          Ada.Text_IO.New_Line(File => File_Handle_Out);
+        end;
+      end loop;
+
+      Ada.Text_IO.Close(File => File_Handle);
+
+    end;
+
+  end;
 
 end data_crunching;
 
